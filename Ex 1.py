6a6356c08dd0987ce5758d11cfce24f48dedc21b
FranzFauni <franzfauni@gmail.com>
2024-10-24 11:09:27 +0400

    Add files via upload

---
+++ Ex 1.py
@@ -0,0 +1,11 @@
+
+#Create a three variables containing strs 
+
+word1= " sprite"
+word2 = " burger"
+word3 = " and fries"
+
+# Use string concatenation to combine the variables and print the phrase
+print(word1 + word2 + word3)
+
+
